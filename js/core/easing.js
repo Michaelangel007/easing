ee5faf93b26d2ebfe8377bda3c81f653c5c12faa
Michaelangel007 <testcraft@sharedcraft.com>
2016-12-31 15:05:01 -0800

    Cleanup alignment for args

--- js/core/easing.js
+++ js/core/easing.js
@@ -93,44 +93,44 @@ var Easing =
 var EasingFuncs = // Array of Functions
 [
 // Power -- grouped by In,Out,InOut
-    function None          (p)  { return 1;               }, // p^0 Placeholder for no active animation
-    function Linear        (p)  { return p;               }, // p^1 Note: In = Out = InOut
-    function InQuadratic   (p)  { return p*p;             }, // p^2 = Math.pow(p,2)
-    function InCubic       (p)  { return p*p*p;           }, // p^3 = Math.pow(p,3)
-    function InQuartic     (p)  { return p*p*p*p;         }, // p^4 = Math.pow(p,4)
-    function InQuintic     (p)  { return p*p*p*p*p;       }, // p^5 = Math.pow(p,5)
-    function InSextic      (p)  { return p*p*p*p*p*p;     }, // p^6 = Math.pow(p,6)
-    function InSeptic      (p)  { return p*p*p*p*p*p*p;   }, // p^7 = Math.pow(p,7)
-    function InOctic       (p)  { return p*p*p*p*p*p*p*p; }, // p^8 = Math.pow(p,8)
+    function None           (p) { return 1;               }, // p^0 Placeholder for no active animation
+    function Linear         (p) { return p;               }, // p^1 Note: In = Out = InOut
+    function InQuadratic    (p) { return p*p;             }, // p^2 = Math.pow(p,2)
+    function InCubic        (p) { return p*p*p;           }, // p^3 = Math.pow(p,3)
+    function InQuartic      (p) { return p*p*p*p;         }, // p^4 = Math.pow(p,4)
+    function InQuintic      (p) { return p*p*p*p*p;       }, // p^5 = Math.pow(p,5)
+    function InSextic       (p) { return p*p*p*p*p*p;     }, // p^6 = Math.pow(p,6)
+    function InSeptic       (p) { return p*p*p*p*p*p*p;   }, // p^7 = Math.pow(p,7)
+    function InOctic        (p) { return p*p*p*p*p*p*p*p; }, // p^8 = Math.pow(p,8)
 
-    function OutQuadratic  (p)  { var m=p-1; return 1-m*m;             },
-    function OutCubic      (p)  { var m=p-1; return 1+m*m*m;           },
-    function OutQuartic    (p)  { var m=p-1; return 1-m*m*m*m;         },
-    function OutQuintic    (p)  { var m=p-1; return 1+m*m*m*m*m;       },
-    function OutSextic     (p)  { var m=p-1; return 1-m*m*m*m*m*m;     },
-    function OutSeptic     (p)  { var m=p-1; return 1+m*m*m*m*m*m*m;   },
-    function OutOctic      (p)  { var m=p-1; return 1-m*m*m*m*m*m*m*m; },
+    function OutQuadratic   (p) { var m=p-1; return 1-m*m;             },
+    function OutCubic       (p) { var m=p-1; return 1+m*m*m;           },
+    function OutQuartic     (p) { var m=p-1; return 1-m*m*m*m;         },
+    function OutQuintic     (p) { var m=p-1; return 1+m*m*m*m*m;       },
+    function OutSextic      (p) { var m=p-1; return 1-m*m*m*m*m*m;     },
+    function OutSeptic      (p) { var m=p-1; return 1+m*m*m*m*m*m*m;   },
+    function OutOctic       (p) { var m=p-1; return 1-m*m*m*m*m*m*m*m; },
 
-    function InOutQuadratic(p)  { var m=p-1,t=p*2; if (t < 1) return p*t;             return 1-m*m            *  2; },
-    function InOutCubic    (p)  { var m=p-1,t=p*2; if (t < 1) return p*t*t;           return 1+m*m*m          *  4; },
-    function InOutQuartic  (p)  { var m=p-1,t=p*2; if (t < 1) return p*t*t*t;         return 1-m*m*m*m        *  8; },
-    function InOutQuintic  (p)  { var m=p-1,t=p*2; if (t < 1) return p*t*t*t*t;       return 1+m*m*m*m*m      * 16; },
-    function InOutSextic   (p)  { var m=p-1,t=p*2; if (t < 1) return p*t*t*t*t*t;     return 1-m*m*m*m*m*m    * 32; },
-    function InOutSeptic   (p)  { var m=p-1,t=p*2; if (t < 1) return p*t*t*t*t*t*t;   return 1+m*m*m*m*m*m*m  * 64; },
-    function InOutOctic    (p)  { var m=p-1,t=p*2; if (t < 1) return p*t*t*t*t*t*t*t; return 1-m*m*m*m*m*m*m*m*128; },
+    function InOutQuadratic (p) { var m=p-1,t=p*2; if (t < 1) return p*t;             return 1-m*m            *  2; },
+    function InOutCubic     (p) { var m=p-1,t=p*2; if (t < 1) return p*t*t;           return 1+m*m*m          *  4; },
+    function InOutQuartic   (p) { var m=p-1,t=p*2; if (t < 1) return p*t*t*t;         return 1-m*m*m*m        *  8; },
+    function InOutQuintic   (p) { var m=p-1,t=p*2; if (t < 1) return p*t*t*t*t;       return 1+m*m*m*m*m      * 16; },
+    function InOutSextic    (p) { var m=p-1,t=p*2; if (t < 1) return p*t*t*t*t*t;     return 1-m*m*m*m*m*m    * 32; },
+    function InOutSeptic    (p) { var m=p-1,t=p*2; if (t < 1) return p*t*t*t*t*t*t;   return 1+m*m*m*m*m*m*m  * 64; },
+    function InOutOctic     (p) { var m=p-1,t=p*2; if (t < 1) return p*t*t*t*t*t*t*t; return 1-m*m*m*m*m*m*m*m*128; },
 
 // Standard -- grouped by Type
-    function InBack        (p)  { var              k = 1.70158        ;              return p*p*(p*(k+1) - k);                                        },
-    function InOutBack     (p)  { var m=p-1,t=p*2, k = 1.70158 * 1.525; if (p < 0.5) return p*t*(t*(k+1) - k); else return 1 + 2*m*m*(2*m*(k+1) + k); }, // NOTE: Can go negative! i.e. p = 0.008
-    function OutBack       (p)  { var m=p-1,       k = 1.70158        ;                                             return 1 +   m*m*(  m*(k+1) + k); },
+    function InBack         (p) { var              k = 1.70158        ;              return p*p*(p*(k+1) - k);                                        },
+    function InOutBack      (p) { var m=p-1,t=p*2, k = 1.70158 * 1.525; if (p < 0.5) return p*t*(t*(k+1) - k); else return 1 + 2*m*m*(2*m*(k+1) + k); }, // NOTE: Can go negative! i.e. p = 0.008
+    function OutBack        (p) { var m=p-1,       k = 1.70158        ;                                             return 1 +   m*m*(  m*(k+1) + k); },
 
-    function InBounce      (p)  { return 1 - EasingFuncs[ Easing.OUT_BOUNCE ]( 1-p ); },
-    function InOutBounce   (p)  {
+    function InBounce       (p) { return 1 - EasingFuncs[ Easing.OUT_BOUNCE ]( 1-p ); },
+    function InOutBounce    (p) {
                                     var t = p*2;
                                     if (t < 1) return 0.5 - 0.5*EasingFuncs[ Easing.OUT_BOUNCE ]( 1 - t );
                                     return            0.5 + 0.5*EasingFuncs[ Easing.OUT_BOUNCE ]( t - 1 );
                                 },
-    function OutBounce     (p)  {
+    function OutBounce      (p) {
                                     var r  = 1  / 2.75; // reciprocal
                                     var k1 =         r; // 36.36%
                                     var k2 = 2     * r; // 72.72%
@@ -146,60 +146,60 @@ var EasingFuncs = // Array of Functions
                                     else             { t = p - k6; return k0 * t*t + 0.984375; } // 63/64
                                 },
 
-    function InCircle      (p)  {                             return  1-Math.sqrt( 1 - p*p );                                                      },
-    function InOutCircle   (p)  { var m=p-1,t=p*2; if (t < 1) return (1-Math.sqrt( 1 - t*t ))*0.5; else return (Math.sqrt( 1 - 4*m*m ) + 1) * 0.5; },
-    function OutCircle     (p)  { var m=p-1      ;                                                      return  Math.sqrt( 1 -   m*m );            },
+    function InCircle       (p) {                             return  1-Math.sqrt( 1 - p*p );                                                      },
+    function InOutCircle    (p) { var m=p-1,t=p*2; if (t < 1) return (1-Math.sqrt( 1 - t*t ))*0.5; else return (Math.sqrt( 1 - 4*m*m ) + 1) * 0.5; },
+    function OutCircle      (p) { var m=p-1      ;                                                      return  Math.sqrt( 1 -   m*m );            },
 
-    function InElastic     (p)  { var m = p-1; return  - Math.pow( 2,10*m  ) * Math.sin( ( m*40 - 3) * Math.PI/6  ); },
-    function InOutElastic  (p)  {
+    function InElastic      (p) { var m = p-1; return  - Math.pow( 2,10*m  ) * Math.sin( ( m*40 - 3) * Math.PI/6  ); },
+    function InOutElastic   (p) {
                                     var s = 2*p-1;                 // remap: [0,0.5] -> [-1,0]
                                     var k = (80*s-9) * Math.PI/18; // and    [0.5,1] -> [0,+1]
 
                                     if (s < 0) return   -0.5*Math.pow(2, 10*s) * Math.sin( k );
                                     else       return 1 +0.5*Math.pow(2,-10*s) * Math.sin( k );
                                 },
-    function OutElastic    (p)  {              return 1+(Math.pow( 2,10*-p ) * Math.sin( (-p*40 - 3) * Math.PI/6 )); },
+    function OutElastic     (p) {              return 1+(Math.pow( 2,10*-p ) * Math.sin( (-p*40 - 3) * Math.PI/6 )); },
 
     // NOTE: 'Exponent2' needs clamping for 0 and 1 respectively
-    function InExponent2   (p)  {   if (p <= 0) return 0; return   Math.pow( 2,  10*(p-1) ); },
-    function InOutExponent2(p)  {
+    function InExponent2    (p) {   if (p <= 0) return 0; return   Math.pow( 2,  10*(p-1) ); },
+    function InOutExponent2 (p) {
                                     if (p <= 0) return 0;
                                     if (p >= 1) return 1;
                                     if (p <0.5) return             Math.pow( 2,  10*(2*p-1)-1);
                                     else        return           1-Math.pow( 2, -10*(2*p-1)-1);
                                 },
-    function OutExponent2  (p)  {   if (p >= 1) return 1; return 1-Math.pow( 2, -10* p    ); },
+    function OutExponent2   (p)  {   if (p >= 1) return 1; return 1-Math.pow( 2, -10* p    ); },
 
 
-    function InSine        (p)  { return      1 - Math.cos( p * Math.PI*0.5 );  },
-    function InOutSine     (p)  { return 0.5*(1 - Math.cos( p * Math.PI     )); },
-    function OutSine       (p)  { return          Math.sin( p * Math.PI*0.5 );  },
+    function InSine         (p) { return      1 - Math.cos( p * Math.PI*0.5 );  },
+    function InOutSine      (p) { return 0.5*(1 - Math.cos( p * Math.PI     )); },
+    function OutSine        (p) { return          Math.sin( p * Math.PI*0.5 );  },
 
 // Non-Standard
-    function InExponentE   (p)  {   if (p <= 0) return 0; return   Math.pow( Math.E, -10*(1-p) ); }, // Scale 0..1 -> p^-10 .. p^0
-    function InOutExponentE(p)  {
+    function InExponentE    (p) {   if (p <= 0) return 0; return   Math.pow( Math.E, -10*(1-p) ); }, // Scale 0..1 -> p^-10 .. p^0
+    function InOutExponentE (p) {
                                     var t = p*2;
                                     if (t < 1) return 0.5 - 0.5*EasingFuncs[ Easing.OUT_EXPONENTE ]( 1 - t );
                                     return            0.5 + 0.5*EasingFuncs[ Easing.OUT_EXPONENTE ]( t - 1 );
                                 },
-    function OutExponentE  (p)  { return 1 - EasingFuncs[ Easing.IN_EXPONENTE ]( 1-p ); },
+    function OutExponentE   (p) { return 1 - EasingFuncs[ Easing.IN_EXPONENTE ]( 1-p ); },
 
 
-    function InLog10       (p)  { return 1 - EasingFuncs[ Easing.OUT_LOG10 ]( 1-p ); },
-    function InOutLog10    (p)  {
+    function InLog10        (p) { return 1 - EasingFuncs[ Easing.OUT_LOG10 ]( 1-p ); },
+    function InOutLog10     (p) {
                                     var t = p*2;
                                     if (t < 1) return 0.5 - 0.5*EasingFuncs[ Easing.OUT_LOG10      ]( 1 - t );
                                     return            0.5 + 0.5*EasingFuncs[ Easing.OUT_LOG10      ]( t - 1 );
                                 },
-    function OutLog10      (p)  { return Math.log10( (p*9)+1 ); }, // Scale 0..1 -> Log10( 1 ) .. Log10( 10 )
+    function OutLog10       (p) { return Math.log10( (p*9)+1 ); }, // Scale 0..1 -> Log10( 1 ) .. Log10( 10 )
 
-    function InSquareRoot  (p)  { return 1 - EasingFuncs[ Easing.OUT_SQRT       ]( 1-p ); },
+    function InSquareRoot   (p) { return 1 - EasingFuncs[ Easing.OUT_SQRT       ]( 1-p ); },
     function InOutSquareRoot(p) {
                                     var t = p*2;
                                     if (t < 1) return 0.5 - 0.5*EasingFuncs[ Easing.OUT_SQRT       ]( 1 - t );
                                     return            0.5 + 0.5*EasingFuncs[ Easing.OUT_SQRT       ]( t - 1 );
                                 },
-    function OutSquareRoot (p)  { return Math.sqrt( p ) },
+    function OutSquareRoot  (p) { return Math.sqrt( p ) },
 
 /*
 // Alternative: Standard -- Grouped by In, Out, InOut
